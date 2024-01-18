1ec7d6430fbc764748347319ddd7f0c1eeb0b3aa
WCatz <waynecataldo@gmail.com>
2024-01-18 12:09:42 -0600

    redo loop

--- tatertots.sh
+++ tatertots.sh
@@ -2,6 +2,7 @@
 
 source miner.conf
 gen_color() { printf "#%06X\n" $((RANDOM % 0xFFFFFF)); }
+declare -g last_hash_result=""
 
 gen_hash() {
     local last_hash=$1 miner_id=$2 color=$3
@@ -12,26 +13,33 @@ gen_hash() {
 
 submit() {
     local miner_id=$1 retries=3
-    for ((i=0; i<$retries; i++)); do
-        local result=$(curl -s "$host/blockchain_config")
-        last_hash_result=$(echo "$result" | jq -r '.last_block.hash')
-        echo "Last Hash: $last_hash_result"
-        local color=$(gen_color)
-        gen_hash "$last_hash_result" "$miner_id" "$color"
-        local response=$(curl -s -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "{\"hash\": \"$hash_result\", \"miner_id\": \"$miner_id_result\", \"color\": \"$color_result\"}" "$host/submit_block")
-        http_status="${response: -3}"
-        [ "$http_status" -eq 200 ] && { echo -e "API Response Code: $http_status\nNew Hash: $hash_result"; return; } || { echo "Attempt $((i+1)) failed - API Request failed with HTTP response code: $http_status"; sleep 1; }
-    done
+    local response=$(curl -s -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "{\"hash\": \"$hash_result\", \"miner_id\": \"$miner_id_result\", \"color\": \"$color_result\"}" "$host/submit_block")
+    local http_status="${response: -3}"
+    if [ "$http_status" -eq 200 ]; then
+        echo -e "API Response Code: $http_status\nNew Hash: $hash_result"
+    else
+        echo "API Request failed with HTTP response code: $http_status"
+    fi
 }
 
 mine() {
-    for miner_id in "${miner_ids[@]}"; do
-        submit "$miner_id"
-        script_pid=$$
-        memory_usage=$(ps -p $script_pid -o rss=)
-        echo "Memory usage of the script: ${memory_usage} KB"
-        echo -e "$(tput setaf 2)Tatertots: mining for $miner_id_result with $color_result$(tput sgr0)\n"
-    done
+    local result=$(curl -s "$host/blockchain_config")
+    local new_last_hash_result=$(echo "$result" | jq -r '.last_block.hash')
+    if [ "$new_last_hash_result" != "$last_hash_result" ]; then
+        last_hash_result="$new_last_hash_result"
+        echo "Last Hash: $last_hash_result"
+        for miner_id in "${miner_ids[@]}"; do
+            local color=$(gen_color)
+            gen_hash "$last_hash_result" "$miner_id" "$color"
+            submit "$miner_id"
+            script_pid=$$
+            memory_usage=$(ps -p $script_pid -o rss=)
+            echo "Memory usage of the script: ${memory_usage} KB"
+            echo -e "$(tput setaf 2)Tatertots: mining for $miner_id_result with $color_result$(tput sgr0)\n"
+        done
+    else
+        echo "Last Hash has not changed. Skipping submission."
+    fi
 }
 
 while true; do mine; sleep 49; done
